1de1c3e4b6a804721d2fdcf6ce19a4c318fbc1b0
capmec <capmec@gmail.com>
2024-09-04 16:38:38 +0200

    Inputs done

---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Form } from '@/components/ui/form';
+import CustomFormField from '@/components/CustomFormField';
+import SubmitButton from '../SubmitButton';
+import { useState } from 'react';
+import { UserFormValidation } from '@/lib/validation';
+
+export enum FormFieldType {
+	INPUT = 'input',
+	TEXTAREA = 'textarea',
+	PHONE_INPUT = 'phoneInput',
+	CHECKBOX = 'checkbox',
+	DATE_PICKER = 'datePicker',
+	SELECT = 'select',
+	SKELETON = 'skeleton',
+}
+
+const PatientForm = () => {
+	const [isLoading, setIsloading] = useState(false);
+
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof UserFormValidation>>({
+		resolver: zodResolver(UserFormValidation),
+		defaultValues: {
+			name: '',
+			email: '',
+			phone: '',
+		},
+	});
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof UserFormValidation>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className='space-y-6 flex-1'>
+				<section className='mb-12 space-y-4'>
+					<h1 className='header'>Welcome! 👩‍⚕️</h1>
+					<p className='text-dark-700'>Schedule your first appointment</p>
+				</section>
+				<CustomFormField
+					fieldType={FormFieldType.INPUT}
+					control={form.control}
+					name='name'
+					label='Full Name'
+					placeholder='Jane Doe'
+					iconSrc='/assets/icons/user.svg'
+					iconAlt='user'
+				/>
+				<CustomFormField
+					fieldType={FormFieldType.INPUT}
+					control={form.control}
+					name='email'
+					label='Email'
+					placeholder='janedoe@example.com'
+					iconSrc='/assets/icons/email.svg'
+					iconAlt='email'
+				/>
+				<CustomFormField
+					fieldType={FormFieldType.PHONE_INPUT}
+					control={form.control}
+					name='phone'
+					label='Phone number'
+					placeholder='(555) 123-4567'
+				/>
+
+				<SubmitButton isLoading={isLoading}> Get Started </SubmitButton>
+			</form>
+		</Form>
+	);
+};
+
+export default PatientForm;
