bc14cef772da1c6fbb429a0c81284340f889a77c
capmec <capmec@gmail.com>
2024-09-09 16:28:37 +0200

    register forme part 1

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -2,6 +2,7 @@
 import { ID, Query } from 'node-appwrite';
 import { users } from '../appwrite.config';
 import { parseStringify } from '../utils';
+import clsx from 'clsx';
 
 export const createUser = async (user: CreateUserParams) => {
 	try {
@@ -22,5 +23,14 @@ export const createUser = async (user: CreateUserParams) => {
 			]);
 			return existingUser.users[0];
 		}
+	}
+};
+
+export const getUser = async (userId: string) => {
+	try {
+		const user = await users.get(userId);
+		return parseStringify(user);
+	} catch (error) {
+		console.log(error);
 	}
 };
