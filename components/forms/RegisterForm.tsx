b3b48fe1cd78b284a7ae5718defa2609d00d90a1
capmec <capmec@gmail.com>
2024-09-10 15:20:00 +0200

    register form UI done

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -1,19 +1,39 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import {
+	ErrorOption,
+	Field,
+	FieldArray,
+	FieldArrayPath,
+	FieldError,
+	FieldErrors,
+	FieldName,
+	FieldRefs,
+	FieldValues,
+	FormState,
+	InternalFieldName,
+	RegisterOptions,
+	SubmitErrorHandler,
+	SubmitHandler,
+	useForm,
+	UseFormRegisterReturn,
+} from 'react-hook-form';
 import { z } from 'zod';
 import { Form, FormControl } from '@/components/ui/form';
 import CustomFormField from '@/components/CustomFormField';
 import SubmitButton from '../SubmitButton';
-import { useState } from 'react';
+import { BaseSyntheticEvent, useState } from 'react';
 import { UserFormValidation } from '@/lib/validation';
 import { useRouter } from 'next/navigation';
 import { createUser } from '@/lib/actions/patient.actions';
 import { FormFieldType } from './PatientForm';
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
-import { GenderOptions } from '@/constants';
+import { Doctors, GenderOptions, IdentificationTypes } from '@/constants';
 import { Label } from '../ui/label';
+import { SelectItem } from '../ui/select';
+import Image from 'next/image';
+import { FileUploader } from '../FileUploader';
 
 const RegisterForm = ({ user }: { user: User }) => {
 	const router = useRouter();
@@ -163,10 +183,143 @@ const RegisterForm = ({ user }: { user: User }) => {
 						<h2 className='sub-header'>Medical Information</h2>
 					</div>
 				</section>
-				<div className='flex flex-col gap-6 xl:flex-row'></div>
-				<div className='flex flex-col gap-6 xl:flex-row'></div>
+				<CustomFormField
+					fieldType={FormFieldType.SELECT}
+					control={form.control}
+					name='primaryPhysician'
+					label='Primary Physician'
+					placeholder='Select a physician'>
+					{Doctors.map((doctor) => (
+						<SelectItem
+							key={doctor.name}
+							value={doctor.name}>
+							<div className='flex cursor-pointer items-center gap-2'>
+								<Image
+									src={doctor.image}
+									width={32}
+									height={32}
+									alt={doctor.name}
+									className='rounded-full border border-dark-500'
+								/>
+								<p>{doctor.name}</p>
+							</div>
+						</SelectItem>
+					))}
+				</CustomFormField>
+				<div className='flex flex-col gap-6 xl:flex-row'>
+					<CustomFormField
+						fieldType={FormFieldType.INPUT}
+						control={form.control}
+						name='insuranceProvider'
+						label='Insurance Provider'
+						placeholder='Techniker Krankenkasse'
+					/>
+					<CustomFormField
+						fieldType={FormFieldType.INPUT}
+						control={form.control}
+						name='insurancePoliceNumber'
+						label='Insurance Policy Number'
+						placeholder='ABC123456789'
+					/>
+				</div>
+				<div className='flex flex-col gap-6 xl:flex-row'>
+					<CustomFormField
+						fieldType={FormFieldType.TEXTAREA}
+						control={form.control}
+						name='allergies'
+						label='Allergies (if any)'
+						placeholder='Peanuts, Sulfa, Penicillin'
+					/>
+					<CustomFormField
+						fieldType={FormFieldType.TEXTAREA}
+						control={form.control}
+						name='currentMedications'
+						label='Current Medications (if any)'
+						placeholder='Aspirin, Tylenol, Ibuprofen'
+					/>
+				</div>
+				<div className='flex flex-col gap-6 xl:flex-row'>
+					<CustomFormField
+						fieldType={FormFieldType.TEXTAREA}
+						control={form.control}
+						name='familyMedicalHistory'
+						label='Family Medical History'
+						placeholder='Mother has Diabetes, Father has Hypertension'
+					/>
+					<CustomFormField
+						fieldType={FormFieldType.TEXTAREA}
+						control={form.control}
+						name='pastMedicalHistory'
+						label='Past Medical History'
+						placeholder='Hypertension, Diabetes, CAD'
+					/>
+				</div>
+				<section className='space-y-6'>
+					<div className='mb-9 space-y-1'>
+						<h2 className='sub-header'>Identification and Verification</h2>
+					</div>
+				</section>
 
-				<SubmitButton isLoading={isLoading}> Get Started </SubmitButton>
+				<CustomFormField
+					fieldType={FormFieldType.SELECT}
+					control={form.control}
+					name='identificationType'
+					label='Identification Type'
+					placeholder='Select an identification type'>
+					{IdentificationTypes.map((type) => (
+						<SelectItem
+							key={type}
+							value={type}>
+							{type}
+						</SelectItem>
+					))}
+				</CustomFormField>
+				<CustomFormField
+					fieldType={FormFieldType.INPUT}
+					control={form.control}
+					name='identificationNumber'
+					label='Identification Number'
+					placeholder='ABC123456789'
+				/>
+				<CustomFormField
+					fieldType={FormFieldType.SKELETON}
+					control={form.control}
+					name='identificationDocument'
+					label='Scanned copy of identification document'
+					renderSkeleton={(field) => (
+						<FormControl>
+							<FileUploader
+								files={field.value}
+								onChange={field.onChange}
+							/>
+						</FormControl>
+					)}
+				/>
+				<section className='space-y-6'>
+					<div className='mb-9 space-y-1'>
+						<h2 className='sub-header'>Consent and Privacy</h2>
+					</div>
+				</section>
+				<CustomFormField
+					fieldType={FormFieldType.CHECKBOX}
+					control={form.control}
+					name='treatmentConsent'
+					label='I consent to treatment'
+				/>
+				<CustomFormField
+					fieldType={FormFieldType.CHECKBOX}
+					control={form.control}
+					name='disclosureConsent'
+					label='I consent to disclosure of my personal information'
+				/>
+				<CustomFormField
+					fieldType={FormFieldType.CHECKBOX}
+					control={form.control}
+					name='privacyConsent'
+					label='I consent to privacy policy'
+				/>
+
+				<SubmitButton isLoading={isLoading}> Submit and continue </SubmitButton>
 			</form>
 		</Form>
 	);
